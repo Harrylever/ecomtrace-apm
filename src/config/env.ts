117dc549d3f00cce53dadf6718db7171ddf5e7b7
Ukanah Dean <ukanah15thdean@gmail.com>
2025-02-20 14:15:19 +0100

    feat: added undefined values for envs

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,8 +6,11 @@ const config = {
 	port: process.env.PORT || 3002,
 	nodeEnv: process.env.NODE_ENV || 'development',
 	logLevel: process.env.LOG_LEVEL || 'info',
-	apmWebsiteUrl: process.env.APP_WEBSITE_URL,
-	apmWebsiteApiUrl: process.env.APM_WEBSITE_API_URL,
+	apmWebsiteUrl:
+		process.env.APP_WEBSITE_URL || 'https://ecomtrace-platform.vercel.app',
+	apmWebsiteApiUrl:
+		process.env.APM_WEBSITE_API_URL ||
+		'https://ecomtrace-platform-production.up.railway.app',
 };
 
 export default config;
