417c0aeee535fe6b6841172e72fd9e4a02ec7848
Ukanah Dean <ukanah15thdean@gmail.com>
2025-02-20 13:54:19 +0100

    feat: testing envs

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 import middleware from './middleware';
 import indexRoutes from './routes/index';
-import repeaterRoutes from './routes/repeater';
 import './config/env';
 
 const app = express();
 
 middleware(app);
 app.use('/', indexRoutes);
-app.use('/repeater', repeaterRoutes);
 
 export default app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import config from '../config/env';
+import axios from 'axios';
+import logger from '../config/logger';
 
 const router = express.Router();
 
@@ -31,7 +33,7 @@ router.get('/integration.json', (req, res) => {
 			],
 			integration_category: 'E-commerce & Retail',
 			author: 'Onesi Ukanah',
-			website: config.apmWebsiteUrl,
+			website: `Website: ${config.apmWebsiteUrl}`,
 			settings: [
 				{
 					label: 'forward-type',
@@ -44,6 +46,46 @@ router.get('/integration.json', (req, res) => {
 			tick_target: `${baseUrl}/target_url`,
 		},
 	});
+});
+
+interface ErrorTracingDto {
+	message?: string;
+	settings?: { label?: string; default?: string }[];
+}
+
+router.post('/target_url', async (req, res) => {
+	const { message, settings } = req.body as ErrorTracingDto;
+	const repeater_type = settings?.filter(
+		(setting) => setting.label === 'forward-type'
+	)?.[0].default;
+
+	if (!repeater_type) {
+		res.status(400).json({
+			error: 'forward-type setting is required',
+		});
+		return;
+	}
+
+	const webhookUrl = `${config.apmWebsiteApiUrl}/receive-repeater?forward_type=${repeater_type}`;
+
+	try {
+		const response = await axios.post(
+			webhookUrl,
+			{
+				message,
+			},
+			{
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		);
+
+		res.status(200).json({ status: 'success', webhookResponse: response.data });
+	} catch (error) {
+		logger.error('Failed to send webhook:', (error as Error).message);
+		res.status(500).json({ error: 'Failed to send webhook' });
+	}
 });
 
 export default router;
